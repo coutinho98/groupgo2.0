5331d9fde4942058298e4a723a9a67902d171989
coutinho98 <mateuscouto98@outlook.com>
2025-03-31 01:27:13 -0300

    feat: i added the function to create an accout

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,89 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import CustomTheme from "@/components/CustomTheme";
 import { UserPlus } from "lucide-react";
+import { toast } from 'sonner'
+import { useState } from "react";
+import { useReward } from 'react-rewards';
+
+interface FormData {
+    username: string;
+    email: string;
+    password: string;
+    repeatPassword: string
+}
 
 export default function Register() {
+    const [formData, setFormData] = useState<FormData>({
+        username: "",
+        email: "",
+        password: "",
+        repeatPassword: ""
+    })
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [errors, setErrors] = useState<string | null>(null);
+    const { reward: confettiReward, isAnimating: isConfettiAnimating } = useReward('confettiReward', 'confetti');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        try {
+            const response = await fetch("https://groupgo.onrender.com/user", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: formData.username,
+                    email: formData.email,
+                    password: formData.password
+                }),
+            });
+
+            setIsLoading(false);
+
+            if (response.ok) {
+                const data: { token: string } = await response.json();
+                console.log(data);
+
+                setTimeout(() => {
+                    toast.success("conta criada", {
+                        duration: 3000,
+                        position: "top-center"
+                    });
+                    confettiReward();
+
+                    setTimeout(() => {
+                        navigate('/perfil');
+                    }, 3500)
+                }, 500)
+            } else {
+                const errorData: { message?: string } = await response.json();
+                setErrors(errorData.message || 'Falha ao criar conta');
+                toast.error(errorData.message || 'Falha ao criar conta', {
+                    duration: 3000,
+                    position: "top-center"
+                });
+            }
+        } catch (err) {
+            setIsLoading(false);
+            setErrors('Erro no servidor');
+            toast.error('Erro no servidor', {
+                duration: 3000,
+                position: "top-center"
+            });
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
 
     return (
         <div className="min-h-screen flex items-center justify-center px-4 bg-background text-foreground">
@@ -14,12 +93,46 @@ export default function Register() {
                     <UserPlus className="h-20 w-20 mb-10 text-foreground" />
                     <h1 className="text-5xl">Criar Conta</h1>
                 </div>
-                <form className="space-y-4">
-                    <Input type="email" placeholder="Email" className="bg-muted/50" />
-                    <Input type="password" placeholder="Senha" className="bg-muted/50" />
-                    <Input type="password" placeholder="Repetir Senha" className="bg-muted/50" />
-                    <Button type="submit" className="w-full">
+                <form className="space-y-4" onSubmit={handleSubmit}>
+                    <Input
+                        type="text"
+                        name="username"
+                        placeholder="Usuário"
+                        className="bg-muted/50"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="bg-muted/50"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Input
+                        type="password"
+                        name="password"
+                        placeholder="Senha"
+                        className="bg-muted/50"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Input
+                        type="password"
+                        name="repeatPassword"
+                        placeholder="Repetir Senha"
+                        className="bg-muted/50"
+                        value={formData.repeatPassword}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Button disabled={isConfettiAnimating} type="submit" className="w-full cursor-pointer">
                         Inscrever-se
+                        <span id="confettiReward" />
                     </Button>
                 </form>
                 <p className="text-center text-sm text-muted-foreground">
