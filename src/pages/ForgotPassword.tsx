22b53777ed0130edf82b04e91de72e7ec1ce67a4
coutinho98 <mateuscouto98@outlook.com>
2025-04-07 12:15:42 -0300

    feat: created a forgotpassword page

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,43 +1,46 @@
-import CustomTheme from "@/components/CustomTheme"
-import { Button } from "@/components/ui/button"
+import CustomTheme from "@/components/CustomTheme";
+import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function ForgotPassword() {
-    return (
-        <div className="min-h-screen flex items-center justify-center px-4 bg-background text-foreground relative">
-             <CustomTheme />
-            <Card className="w-[350px]">
-                <CardHeader>
-                    <CardTitle>Create project</CardTitle>
-                    <CardDescription>Deploy your new project in one-click.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="name">Name</Label>
-                                <Input id="name" placeholder="Name of your project" />
-                            </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="framework">Framework</Label>
-                            </div>
-                        </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <Button variant="outline">Cancel</Button>
-                    <Button>Deploy</Button>
-                </CardFooter>
-            </Card>
-        </div>
-    )
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4 bg-background text-foreground relative">
+      <CustomTheme />
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle className="text-4xl whitespace-nowrap">Esqueceu sua senha?</CardTitle>
+          <CardDescription>
+            Digite o e-mail ou @nomedeusuário para alterar sua senha
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">E-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="@nomedeusuario ou e-mail"
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button type="submit">Enviar</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
